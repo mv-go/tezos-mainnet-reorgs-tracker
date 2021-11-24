5a8d4e4c3c53ccd0f2168c7ef0ddf9fa516a0d8c
mv-go <mv.gololobov@gmail.com>
2021-11-24 18:00:25 +0300

    style(eslint): allow multi-line comma-dangle

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,11 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'comma-dangle': [
+      'error',
+      'always-multiline'
+    ]
   },
   overrides: [
     {
