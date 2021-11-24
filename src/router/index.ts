edbb3425b31864fcdb27a261d0a1abfbc32bcc2b
mv-go <mv.gololobov@gmail.com>
2021-11-24 17:58:32 +0300

    style(router): linter autofix

--- src/router/index.ts
+++ src/router/index.ts
@@ -15,14 +15,14 @@ const routes: Array<RouteConfig> = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  },
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes,
 })
 
 export default router
